9d6d6c6244af7823235c95d7a9b7a0baaf850c8e
Aiko <aikocute@icloud.com>
2022-04-29 18:31:12 +0700

    Update

--- api/proxypanel/proypanel_test.go
+++ api/proxypanel/proypanel_test.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/XrayR-project/XrayR/api"
-	"github.com/XrayR-project/XrayR/api/proxypanel"
+	"github.com/AikoCute/XrayR/api"
+	"github.com/AikoCute/XrayR/api/proxypanel"
 )
 
 func CreateClient() api.API {
--- api/v2board/v2board_test.go
+++ api/v2board/v2board_test.go
@@ -3,8 +3,8 @@ package v2board_test
 import (
 	"testing"
 
-	"github.com/XrayR-project/XrayR/api"
-	"github.com/XrayR-project/XrayR/api/v2board"
+	"github.com/AikoCute/XrayR/api"
+	"github.com/AikoCute/XrayR/api/v2board"
 )
 
 func CreateClient() api.API {
