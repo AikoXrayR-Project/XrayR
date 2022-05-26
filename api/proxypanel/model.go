371bf315b3a471a554698a1f1723321a3a132051
Aiko <aikocute@icloud.com>
2022-05-27 00:36:14 +0700

    Update Proxy Panel

--- api/proxypanel/model.go
+++ api/proxypanel/model.go
@@ -64,15 +64,17 @@ type NodeOnline struct {
 }
 
 type VMessUser struct {
-	UID        int    `json:"uid"`
-	VmessUID   string `json:"vmess_uid"`
-	SpeedLimit uint64 `json:"speed_limit"`
+	UID         int    `json:"uid"`
+	VmessUID    string `json:"vmess_uid"`
+	SpeedLimit  uint64 `json:"speed_limit"`
+	DeviceLimit int    `json:"device_limt"`
 }
 
 type TrojanUser struct {
-	UID        int    `json:"uid"`
-	Password   string `json:"password"`
-	SpeedLimit uint64 `json:"speed_limit"`
+	UID         int    `json:"uid"`
+	Password    string `json:"password"`
+	SpeedLimit  uint64 `json:"speed_limit"`
+	DeviceLimit int    `json:"device_limt"`
 }
 
 type SSUser struct {
